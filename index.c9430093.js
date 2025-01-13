3f52988020f923036eb835ea22fcceee9a8988e1
Yuliia Nudyk <yulyayu833@gmail.com>
2025-01-13 21:56:30 +0200

    Updates

--- index.c9430093.js
+++
@@ -1,2 +0,0 @@
-var e=document.querySelector(".gallery__large-img");document.querySelector(".gallery__list").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest(".list-item__link");r&&e&&(e.src=r.href)});
-//# sourceMappingURL=index.c9430093.js.map
