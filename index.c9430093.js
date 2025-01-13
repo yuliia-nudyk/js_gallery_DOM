806d97254a0ce46ca341d6bdb52b6036f56c487d
Yuliia Nudyk <yulyayu833@gmail.com>
2025-01-13 21:55:40 +0200

    make build

---
+++ index.c9430093.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__large-img");document.querySelector(".gallery__list").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest(".list-item__link");r&&e&&(e.src=r.href)});
+//# sourceMappingURL=index.c9430093.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const mainImage = document.querySelector(".gallery__large-img");
+const galleryList = document.querySelector(".gallery__list");
+galleryList.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const listItem = e.target.closest(".list-item__link");
+    if (!listItem || !mainImage) return;
+    mainImage.src = listItem.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
